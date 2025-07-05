7b825449a97d9b1e81b18712eb00de942df1b010
chen <xiaokeai1833103@163.com>
2025-07-05 17:02:06 +0800

    feat: 配置ui库的vite配置

---
+++ packages/ui/src/components/Button/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="button">提交</button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  width: 100px;
+  height: 30px;
+  border: 1px solid blue;
+  border-radius: 20px;
+}
+</style>
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: ['./src/main.ts'],
+            name: 'ChenUI',
+            formats: ["cjs", 'es'],
+            fileName: (format) => `chen-ui.${format}.js`
+        },
+        rollupOptions: {
+            external: ['vue'],
+        }
+    }
 })
