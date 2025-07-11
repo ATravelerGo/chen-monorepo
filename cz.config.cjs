2e4407865249134dedd72f4c29279f26997fb2c8
Chen <xiaokeai1833103@163.com>
2025-07-11 13:52:10 +0800

    feat: 11

--- cz.config.cjs
+++ cz.config.cjs
@@ -21,7 +21,7 @@ module.exports = defineConfig({
 			confirmCommit: 'Are you sure you want to proceed with the commit above?',
 		},
 		types: [
-			{ value: 'feat', name: 'feat:     ✨  A new feature', emoji: ':sparkles:' },
+			{ value: 'feat', name: 'feat:     ✨  11A new feature', emoji: ':sparkles:' },
 			{ value: 'fix', name: 'fix:      🐛  A bug fix', emoji: ':bug:' },
 			{ value: 'docs', name: 'docs:     📝  Documentation only changes', emoji: ':memo:' },
 			{ value: 'style', name: 'style:    💄  Changes that do not affect the meaning of the code', emoji: ':lipstick:' },
