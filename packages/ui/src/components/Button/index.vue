8d82045ea4b59c3c8dae9851d9a7548efa230a28
Chen <xiaokeai1833103@163.com>
2025-07-14 09:47:36 +0800

    feat: button组件开发

--- packages/ui/src/components/Button/index.vue
+++ packages/ui/src/components/Button/index.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const count = ref(0);
 </script>
 
 <template>
-  <button class="button">提交</button>
+	<button @click="count++" class="button">{{ count }}</button>
 </template>
 
 <style scoped lang="scss">
 .button {
-  width: 100px;
-  height: 30px;
-  border: 1px solid blue;
-  border-radius: 20px;
+	width: 100px;
+	height: 30px;
+	border: 1px solid blue;
+	border-radius: 20px;
 }
 </style>
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	// resolve: {
+	// 	dedupe: ['vue']
+	// },
 	build: {
 		lib: {
 			entry: ['./src/main.ts'],
